1bd8ac7d5684e450c506a6f1a6087be6606e2a15
Akshay <kamblea635@gmail.com>
2023-03-18 11:26:53 +0530

    update typography store

--- src/store/TypographyStore.ts
+++ src/store/TypographyStore.ts
@@ -1,6 +1,25 @@
 import { create } from "zustand";
-import { FontFamily, TypographyTypes } from "../types/TypographyStoreTypes";
+import {
+  FontFamily,
+  TextTypography,
+  TypographyTypes,
+} from "../types/TypographyStoreTypes";
 export const useThemeStore = create<TypographyTypes>()((set, get) => ({
   fontFamily: FontFamily.Roboto,
+  subTitle: {
+    fontSize: 16,
+    Bold: false,
+  },
+  links: {
+    fontSize: 16,
+    Bold: false,
+  },
+  normalText: {
+    fontSize: 16,
+    Bold: false,
+  },
   setFontFamily: (fontFamily: FontFamily) => set({ fontFamily }),
+  setSubTitle: (subTitle: TextTypography) => set({ subTitle }),
+  setLinks: (links: TextTypography) => set({ links }),
+  setNormalText: (normalText: TextTypography) => set({ normalText }),
 }));
--- src/types/TypographyStoreTypes.ts
+++ src/types/TypographyStoreTypes.ts
@@ -5,7 +5,17 @@ export enum FontFamily {
   Montserrat = "Montserrat",
   Inter = "Inter",
 }
+export interface TextTypography {
+  fontSize: number;
+  Bold: boolean;
+}
 export interface TypographyTypes {
   fontFamily: FontFamily;
+  subTitle: TextTypography;
+  links: TextTypography;
+  normalText: TextTypography;
   setFontFamily: (fontFamily: FontFamily) => void;
+  setSubTitle: (subTitle: TextTypography) => void;
+  setLinks: (links: TextTypography) => void;
+  setNormalText: (normalText: TextTypography) => void;
 }
