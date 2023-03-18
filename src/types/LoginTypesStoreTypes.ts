4ccecacd746ee707ef8f8d5084370d7d05f569b1
Akshay <kamblea635@gmail.com>
2023-03-18 14:23:18 +0530

    add logintypes store

---
+++ src/types/LoginTypesStoreTypes.ts
@@ -0,0 +1,24 @@
+export interface SocialLoginButton {
+  icon: string;
+  name: string;
+}
+export interface Approval {
+  name: string;
+  link: string;
+}
+export enum LoginMethods {
+  MAGIC_LINK = "magic_link",
+  MAGIC_CODE = "magic_code",
+  PASSWORD = "password",
+}
+export interface LoginTypesStore {
+  socialLoginTypes: SocialLoginButton[];
+  loginMethods: LoginMethods;
+  approvals: Approval[];
+  setSocialLoginTypes: (socialLoginType: SocialLoginButton) => void;
+  setLoginMethods: (loginMethod: LoginMethods) => void;
+  setApprovals: (approval: Approval) => void;
+  getSocialLoginTypes: () => SocialLoginButton[];
+  getApprovals: () => Approval[];
+  getLoginMethods: () => LoginMethods;
+}
