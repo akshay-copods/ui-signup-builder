6a96a8f04719edd81cf45307d4406522f492d50e
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-19 01:40:54 +0530

    style: pointer events

--- src/components/SignupBuilderContainer/BuilderSidebar/LoginTypes/LoginMethods.tsx
+++ src/components/SignupBuilderContainer/BuilderSidebar/LoginTypes/LoginMethods.tsx
@@ -5,24 +5,24 @@ export const LoginMethod = () => {
   const { getLoginMethods, loginMethods, setLoginMethods } =
     useLoginTypesStore();
   return (
-    <div className='flex gap-3 p-6  flex-col'>
-      <h2 className='text-sm font-medium text-[#000000d9]'>
+    <div className="flex gap-3 p-6  flex-col">
+      <h2 className="text-sm font-medium text-[#000000d9]">
         Work Email & Password-less Logins
       </h2>
-      <div className='flex flex-col gap-3'>
+      <div className="flex flex-col gap-3">
         {emailPassData.map((data: any) => {
           return (
             <div key={data.value}>
-              <div className='flex items-center gap-3'>
+              <label className="flex items-center gap-3 cursor-pointer max-w-max">
                 <input
-                  type='radio'
-                  name='login-methods'
+                  type="radio"
+                  name="login-methods"
                   value={data.value}
                   checked={loginMethods === data.value}
                   onChange={() => setLoginMethods(data.value)}
                 />
-                <span className='font-normal text-xs'>{data.label}</span>
-              </div>
+                <span className="font-normal text-xs">{data.label}</span>
+              </label>
             </div>
           );
         })}
--- src/components/SignupBuilderContainer/BuilderSidebar/LoginTypes/SocialLogin.tsx
+++ src/components/SignupBuilderContainer/BuilderSidebar/LoginTypes/SocialLogin.tsx
@@ -9,38 +9,35 @@ export const SocialLogin = () => {
     useLoginTypesStore();
   console.log("socialLoginTypes", socialLoginTypes);
   return (
-    <div className='flex p-6  flex-col gap-3'>
+    <div className="flex p-6  flex-col gap-3">
       <div>
-        <h2 className='text-sm font-medium text-[#000000d9]'>
+        <h2 className="text-sm font-medium text-[#000000d9]">
           Social Logins ({getSocialLoginTypes().length}/4)
         </h2>
-        <span className='text-xs font-normal text=[#00000073]'>
+        <span className="text-xs font-normal text=[#00000073]">
           You can select only 4 social logins.
         </span>
       </div>
-      <div className='flex flex-col gap-5'>
+      <div className="flex flex-col gap-5">
         {socialButtonOptions.map((data: any) => {
           return (
-            <div
-              key={data.name}
-              className='flex items-center gap-3'>
-              <input
-                className='w-4 h-4'
-                onChange={() => setSocialLoginTypes(data)}
-                value={data.name}
-                checked={getSocialLoginTypes()
-                  .map((k) => k.name)
-                  .includes(data.name)}
-                type='checkbox'
-                name={data.name}
-              />
-              <div className='flex items-center gap-1'>
-                <Icon
-                  className='w-5 h-5'
-                  icon={data.icon}
+            <div key={data.name}>
+              <label className="flex items-center gap-3 cursor-pointer max-w-max">
+                <input
+                  className="w-4 h-4 cursor-pointer"
+                  onChange={() => setSocialLoginTypes(data)}
+                  value={data.name}
+                  checked={getSocialLoginTypes()
+                    .map((k) => k.name)
+                    .includes(data.name)}
+                  type="checkbox"
+                  name={data.name}
                 />
-                <span className='font-normal text-xs'>{data.name}</span>
-              </div>
+                <div className="flex items-center gap-1">
+                  <Icon className="w-5 h-5" icon={data.icon} />
+                  <span className="font-normal text-xs">{data.name}</span>
+                </div>
+              </label>
             </div>
           );
         })}
