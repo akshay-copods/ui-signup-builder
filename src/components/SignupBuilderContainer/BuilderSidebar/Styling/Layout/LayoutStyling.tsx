b1ead00dbfe42b22aa7cdee885fdfb1104fb00a0
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-19 19:17:19 +0530

    add: layout width slider

--- src/components/SignupBuilderContainer/BuilderSidebar/Styling/Layout/LayoutStyling.tsx
+++ src/components/SignupBuilderContainer/BuilderSidebar/Styling/Layout/LayoutStyling.tsx
@@ -6,12 +6,14 @@ import { Icon } from "@iconify/react";
 import { customLayoutData } from "../../../../../stylingConfig";
 
 export const LayoutStyling = () => {
-  const [color, setColor] = useState("#000000");
-  const { gridLayout, setGrid, contentBackground } = useLayoutStore();
+  const [sliderValue, setSldierValue] = useState(50);
+  const { gridLayout, gridContentWidth, setGrid, setGridContentWidth } =
+    useLayoutStore();
   const {
     theme: { backgroundColor },
     changeBackgroundColor,
   } = useThemeStore();
+
   return (
     <div className="p-5 pt-0 flex flex-col gap-4 text-black">
       <div className="flex justify-between">
@@ -32,34 +34,63 @@ export const LayoutStyling = () => {
           />
         </div>
       </div>
+      <div className="flex flex-col w-full border-dashed border-b border-[#0000000f] pb-5">
+        <div className="flex">
+          <input
+            type="range"
+            name="slider"
+            className="flex-1"
+            min={0}
+            max={100}
+            step={10}
+            value={gridContentWidth}
+            onChange={(e) =>
+              Number(e.target.value) <= 60 &&
+              Number(e.target.value) >= 30 &&
+              setGridContentWidth(e.target.value)
+            }
+          />
+          <span>{gridContentWidth}</span>
+        </div>
+      </div>
       <div className="flex border-dashed border-b border-[#0000000f] pb-5 flex-wrap gap-3">
-        {customLayoutData.map((data: any, index: number) => {
-          return (
-            <div key={data.value} className="flex flex-col gap-3 max-w-min">
-              <div
-                // onClick={() => updateCheckStatus(index)}
-                className={`w-88 h-88 ${
-                  data.checked ? "border-blue-600" : "border-gray-100"
-                } border-gray-100 border shadow-md rounded relative`}
-              >
-                <input
-                  className=" border-gray-300 absolute top-1.5 left-1.5 bg-gray-200"
-                  value={data.value}
-                  checked={data.checked}
-                  type="checkbox"
-                  name={data.value}
-                />
+        {customLayoutData.map(
+          (
+            data: {
+              value: string;
+              checked: boolean;
+              label: string;
+              logo?: React.ReactNode;
+            },
+            index: number
+          ) => {
+            return (
+              <div key={data.value} className="flex flex-col gap-3 max-w-min">
+                <div
+                  // onClick={() => updateCheckStatus(index)}
+                  className={`w-88 h-88 ${
+                    data.checked ? "border-blue-600" : "border-gray-100"
+                  } border-gray-100 border shadow-md rounded relative`}
+                >
+                  <input
+                    className=" border-gray-300 absolute top-1.5 left-1.5 bg-gray-200"
+                    value={data.value}
+                    checked={data.checked}
+                    type="checkbox"
+                    name={data.value}
+                  />
+                </div>
+                <div className="gap-1 flex items-center">
+                  {" "}
+                  <span className="font-normal whitespace-normal text-xs text-customBlack-400">
+                    {data.label}
+                  </span>
+                  <span className="text-[#1890FF]">{data.logo}</span>
+                </div>
               </div>
-              <div className="gap-1 flex items-center">
-                {" "}
-                <span className="font-normal whitespace-normal text-xs text-customBlack-400">
-                  {data.label}
-                </span>
-                <span className="text-[#1890FF]">{data.logo}</span>
-              </div>
-            </div>
-          );
-        })}
+            );
+          }
+        )}
       </div>
       <div>
         <div className="flex justify-between">
--- src/store/LayoutStore.ts
+++ src/store/LayoutStore.ts
@@ -3,7 +3,7 @@ import { Grid, LayoutState, ViewPort } from "../types/LayoutStoreTypes";
 
 export const useLayoutStore = create<LayoutState>()((set, get) => ({
   gridLayout: Grid.SPLIT,
-  gridContentWidth: "100%",
+  gridContentWidth: "50",
   contentBackground: "#120338",
   viewPort: ViewPort.DESKTOP,
   content: [
@@ -47,7 +47,8 @@ export const useLayoutStore = create<LayoutState>()((set, get) => ({
     return {
       gridLayout: get().gridLayout,
       contentBackground: get().contentBackground,
-      viewPort:get().viewPort
+      viewPort:get().viewPort,
+      gridContentWidth: get().gridContentWidth,
     };
   },
   setViewPort(viewPort) {
