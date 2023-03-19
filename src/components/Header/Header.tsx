4dc929c236b5bf8d1092fde085969f0270027a98
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-19 13:11:05 +0530

    add: save functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,58 @@
 import React from "react";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import {
+  useButtonStore,
+  useInputFieldStore,
+  useLayoutStore,
+  useLoginTypesStore,
+  useThemeStore,
+  useTypographyStore,
+} from "../../store";
 
 export const Header = () => {
+  const { theme } = useThemeStore();
+  const { socialButton, submitButton } = useButtonStore();
+  const { getInputFieldData } = useInputFieldStore();
+  const { getTypographyStyles } = useTypographyStore();
+  const { getAllLoginTypes } = useLoginTypesStore();
+  const { getLayoutData } = useLayoutStore();
+
+  const saveConfig = async (e) => {
+    e.preventDefault();
+
+    try {
+      const docRef = await addDoc(collection(db, "themeConfig"), {
+        themeConfig: {
+          theme,
+          socialButton,
+          submitButton,
+          inputField: getInputFieldData(),
+          typography: getTypographyStyles(),
+          loginTypes: getAllLoginTypes(),
+          layout: getLayoutData(),
+        },
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
   return (
-    <div className='px-5 py-4 flex justify-start border-b border-customBorderColor-100'>
-      <h3 tabIndex={0} aria-label="Sign Up Builder" className='text-2xl text-customBlack-600 font-medium'>&lt; Sign Up Builder</h3>
+    <div className="px-5 py-4 flex justify-start border-b border-customBorderColor-100">
+      <h3
+        tabIndex={0}
+        aria-label="Sign Up Builder"
+        className="text-2xl text-customBlack-600 font-medium"
+      >
+        &lt; Sign Up Builder
+      </h3>
+      <button
+        onClick={saveConfig}
+        className="text-white px-2 py-1 border rounded-md bg-geekblue-600 ml-auto"
+      >
+        Save
+      </button>
     </div>
   );
 };
