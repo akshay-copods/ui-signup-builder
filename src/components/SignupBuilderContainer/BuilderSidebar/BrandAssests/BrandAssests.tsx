fcc2081714bfdb30efdd4834c72e3ec77c108e73
yash6102000 <yash.thakur@copods.co>
2023-03-21 14:44:40 +0530

    brand assest tab added

---
+++ src/components/SignupBuilderContainer/BuilderSidebar/BrandAssests/BrandAssests.tsx
@@ -0,0 +1,54 @@
+import { Menu, MenuProps } from "antd";
+import React, { useState } from "react";
+import { LogoImageStyling } from "../../..";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+// submenu keys of first level
+const rootSubmenuKeys = ["sub1", "sub2", "sub3"];
+
+export const BrandAssests = ({setEditMenuOpen}:{setEditMenuOpen:(e:boolean)=>void}) => {
+  const [openKeys, setOpenKeys] = useState(["sub1"]);
+  const items: MenuItem[] = [
+    getItem("Logo Image", "sub1", "", [getItem(<LogoImageStyling />, "1")]),
+   
+  ];
+
+  const onOpenChange: MenuProps["onOpenChange"] = (keys: any) => {
+    const latestOpenKey = keys.find((key: any) => openKeys.indexOf(key) === -1);
+    if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
+  return (
+    <div>
+     
+        <Menu
+          mode="inline"
+          openKeys={openKeys}
+          onOpenChange={onOpenChange}
+          style={{ width: "auto", fontSize: "14px" }}
+          items={items}
+        />
+      
+    </div>
+  );
+};
