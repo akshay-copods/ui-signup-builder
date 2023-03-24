d005077158e198524eccb80a7fd589ed9615728d
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-24 12:46:02 +0530

    test: commit check

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import { SignupBuilder } from "./pages/SignupBuilder/SignupBuilder";
 import { BrowserRouter } from "react-router-dom";
+//this is test commit
 const App = () => (
   <BrowserRouter>
     <div className="h-screen">
       <SignupBuilder />
-      
     </div>
   </BrowserRouter>
 );
